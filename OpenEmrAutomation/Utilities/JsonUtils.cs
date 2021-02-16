c184ca2740cd5be0b7a11cc0b1a80dec692c34f9
balaji-githubstore <balaji0017@gmail.com>
2021-02-17 00:57:25 +0530

    Add project files.

---
+++ OpenEmrAutomation/Utilities/JsonUtils.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace OpenEmrAutomation.Utilities
+{
+    public class JsonUtils
+    {
+        public static String GetValue(string key)
+        {
+
+            string path = AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.IndexOf("bin"));
+            StreamReader reader = new StreamReader(path + "/testdata/app.json");
+            string data = reader.ReadToEnd();
+
+            dynamic jdata = JsonConvert.DeserializeObject(data);
+            string value = Convert.ToString(jdata[key]);
+            
+            return value;
+        }
+
+        public void JsonIntoObjectArray(string path,string key)
+        {
+            path = AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.IndexOf("bin")) + "/testdata/app.json";
+            StreamReader reader = new StreamReader(path);
+            string data = reader.ReadToEnd();
+
+            dynamic jdata = JsonConvert.DeserializeObject(data);
+
+            object[] main = new object[jdata[key].Count];
+
+            for (int i = 0; i < main.Length; i++)
+            {
+                object[] temp = new object[jdata[key][i].Count];
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    temp[j] = Convert.ToString(jdata[key][i][j]);
+                }
+                main[i] = temp;
+            }
+            Console.WriteLine(main);
+        }
+    }
+}
